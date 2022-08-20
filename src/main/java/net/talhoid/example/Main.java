e81ceb700bfbe1153ac72c515d4557ee737e198f
Talhoid <68508139+Talhoid@users.noreply.github.com>
2022-08-20 08:07:29 -0400

    fixed sime issues

---
+++ src/main/java/net/talhoid/example/Main.java
@@ -0,0 +1,17 @@
+package net.talhoid.example;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("metora");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("initialized!");
+	}
+}
--- src/main/java/net/talhoid/example/mixin/TitleScreenMixin.java
+++ src/main/java/net/talhoid/example/mixin/TitleScreenMixin.java
@@ -1,16 +1,18 @@
-package net.fabricmc.example.mixin;
+package net.talhoid.example.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.talhoid.example.Main;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.awt.Desktop;
+import java.net.URI;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class TitleScreenMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+	private void init(CallbackInfo info) throws Exception {
+		Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
 	}
 }
