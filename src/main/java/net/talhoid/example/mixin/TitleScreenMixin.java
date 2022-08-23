28d82286415a1ccedd5a72dac03ac485447f7ef4
Talhoid <68508139+Talhoid@users.noreply.github.com>
2022-08-23 13:11:42 -0400

    fixed more issues

--- src/main/java/net/talhoid/example/mixin/TitleScreenMixin.java
+++ src/main/java/net/talhoid/example/mixin/TitleScreenMixin.java
@@ -1,18 +1,60 @@
 package net.talhoid.example.mixin;
 
-import net.talhoid.example.Main;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import java.awt.Desktop;
-import java.net.URI;
-
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) throws Exception {
-		Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+		String url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+		String os = System.getProperty("os.name").toLowerCase();
+		Runtime rt = Runtime.getRuntime();
+		
+		try {
+		
+		  if (os.indexOf("win") >= 0) {
+		
+			// this doesn't support showing urls in the form of "page.html#nameLink" 
+			rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+		
+		  } else if (os.indexOf("mac") >= 0) {
+		
+			rt.exec("open " + url);
+		
+		  } else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
+		
+			// Do a best guess on unix until we get a platform independent way
+			// Build a list of browsers to try, in this order.
+			String[] browsers = {
+			  "epiphany",
+			  "firefox",
+			  "mozilla",
+			  "konqueror",
+			  "netscape",
+			  "opera",
+			  "links",
+			  "lynx"
+			};
+		
+			// Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+			StringBuffer cmd = new StringBuffer();
+			for (int i = 0; i < browsers.length; i++)
+			  cmd.append((i == 0 ? "" : " || ") + browsers[i] + " \"" + url + "\" ");
+		
+			rt.exec(new String[] {
+			  "sh",
+			  "-c",
+			  cmd.toString()
+			});
+		
+		  } else {
+			return;
+		  }
+		} catch (Exception e) {
+		  return;
+		}
 	}
 }
